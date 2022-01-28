a4f2e9207c833a39da2783498717bd88c19d173e
Anna-Sergeevna <4042808@gmail.com>
2022-01-28 16:25:18 +0200

    add phone pattern

--- models/contact.js
+++ models/contact.js
@@ -1,8 +1,8 @@
 const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 
-// const phoneRegexp =
-//   /^\+?.\(?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+const phoneRegexp =
+  /^\+?.\(?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
 
 const contactSchema = Schema(
   {
@@ -18,7 +18,7 @@ const contactSchema = Schema(
     },
     phone: {
       type: String,
-      // match: phoneRegexp,
+      match: phoneRegexp,
       required: true,
     },
     favorite: {
@@ -32,9 +32,7 @@ const contactSchema = Schema(
 const joiSchema = Joi.object({
   name: Joi.string().min(1).max(30).required(),
   email: Joi.string().required(),
-  // phone: Joi.string().pattern(phoneRegexp).required(),
-  phone: Joi.string().required(),
-
+  phone: Joi.string().pattern(phoneRegexp).required(),
   favorite: Joi.bool(),
 });
 
